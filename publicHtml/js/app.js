2397468c393b5a0283f1e8ba841fc478da39b259
Yuji Mori <root@localhost.localdomain>
2013-09-29 14:36:01 +0000

    lean Backbone

---
+++ publicHtml/js/app.js
@@ -0,0 +1,30 @@
+define([
+    "backbone"
+], function (
+    Backbone
+) {
+    //Model
+    var Task = Backbone.Model.extend({
+        defaults : {
+            title: "do something!",
+            completed: false
+        }
+    });
+    var task = new Task();
+
+    //view
+    var TaskView = Backbone.View.extend({
+        tagName: 'li',
+        //className: 'liClass',
+        //id:        'liId',
+        template: _.template("<%- title %>"),
+        render : function () {
+            var template = this.template( this.model.toJSON() );
+            this.$el.html(template);
+            return this;
+        }
+    });
+    var taskView = new TaskView({ model: task });
+    console.log(taskView.render().el);
+    return;
+});
--- publicHtml/js/require.config.js
+++ publicHtml/js/require.config.js
@@ -10,7 +10,7 @@ require.config ({
     "underscore" : "vendor/underscore/underscore",
     "json2"      : "vendor/json2/json2",
     "backbone"   : "vendor/backbone/backbone"
-  },
+},
 
   shim: {
     'underscore' : {
@@ -31,63 +31,4 @@ require.config ({
   }
 });
 
-define(["underscore", "jquery", "backbone"],
-       function () {
-         console.log("start");
-
-         var numbers = [1,2,3,4,5];
-         var persons = [ 
-           { 
-           name : "jonny", 
-           age : 24, 
-           birth_month : 8, 
-           gender : 0 // 0 : male, 1, female 
-          }, 
-          { 
-           name : "paul", 
-           age : 52, 
-           birth_month : 7, 
-           gender : 1 
-          }, 
-          { 
-           name : "under score", 
-           age : 32, 
-           birth_month : 8, 
-           gender : 0 
-          } 
-         ];
-
-         _.each(numbers, function (number) {
-           console.log("number : " + number);
-         });
-
-         var even = _.find(numbers, function (number) {
-           return number % 2 === 0;
-         });
-         console.log("even : " + even);
-
-
-         var evens = _.filter(numbers, function (number) {
-           return number % 2 === 0;
-         });
-
-         console.log(evens);
-
-         var males = _.where(persons, {gender : 0});
-         console.log(JSON.stringify(males));
-
-         var lodestPerson = _.max(persons, function (person) {
-           return person.age;
-         });
-         console.log(lodestPerson);
-
-         var orderByYoung = _.sortBy(persons, function (person) {
-           return person.age;
-         });
-         console.log(orderByYoung);
-
-         console.log("end");
-         return ;
-       
-});
 
